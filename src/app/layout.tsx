7939ae3d520679b238dc71b320d1c1f6d06f5710
selsebil-ben <selsebilben.dev@gmail.com>
2024-09-04 16:06:41 +0200

    add js 4 bootstrap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import BootstrapClient from '@/components/BootstrapClient';
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -18,7 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
+        <BootstrapClient/>
+      </body>
     </html>
   );
 }
